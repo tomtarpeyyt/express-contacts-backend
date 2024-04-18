7f63085c37cfe0df6acb00d148cd3f13d3168bc3
tomtarpeyyt <ttarpey58@gmail.com>
2024-04-18 19:04:01 +0100

    added login for user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,16 +3,15 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('../../config/config');
 
-exports.register = async (req,res) => {
+exports.register = async (req, res) => {
     try {
         const { username, email, password } = req.body;
 
         const existingUser = await User.findOne({ email });
-
         if (existingUser) {
-            return res.status(400).json({ message: 'Email already exists'});
+            return res.status(400).json({ message: 'Email already exists' });
         }
-
+        
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const newUser = new User({
@@ -20,11 +19,33 @@ exports.register = async (req,res) => {
             email,
             password: hashedPassword
         });
-
+        
         await newUser.save();
 
-        res.status(201).json({ message: 'User created successfully'});
+        res.status(201).json({ message: 'User registered successfully' });
     } catch (err) {
-        res.status(500).json({ message: 'Internal server error'});
+        res.status(500).json({ message: 'Internal server error' })
     }
-}
+};
+
+exports.login = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: 'Invalid email or password' });
+      }
+  
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        return res.status(400).json({ message: 'Invalid email or password' });
+      }
+  
+      const token = jwt.sign({ userId: user._id }, config.secretKey, { expiresIn: '1h' });
+  
+      res.status(200).json({ token });
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  };
