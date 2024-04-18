b6927af34d269dc36c9df9e879ae0d3da022341a
tomtarpeyyt <ttarpey58@gmail.com>
2024-04-18 03:33:09 +0100

    sanity check smoke test for express api

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("time2code");
+const express = require('express');
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json('time2code!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
