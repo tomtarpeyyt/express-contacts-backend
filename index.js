ef427ca1e0a5e372e8830174c05ffc6763678c59
tomtarpeyyt <ttarpey58@gmail.com>
2024-04-18 03:36:40 +0100

    setup connection with mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/config');
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(config.dbUrl)
+.then(() => {
+    console.log('connected to MongoDB');
+})
+.catch((err) => {
+    console.error('Error connection to MongoDB', err);
+});
 
 app.use(express.json());
 
