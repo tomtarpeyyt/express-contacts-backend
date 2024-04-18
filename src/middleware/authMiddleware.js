32f47fc80f40744e001296e4dc51c193fcf5bfbf
tomtarpeyyt <ttarpey58@gmail.com>
2024-04-18 19:03:35 +0100

    created auth middleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const config = require('../../config/config');
+const User = require('../models/User');
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    if (!token) {
+      return res.status(401).json({ message: 'Authorization token is missing' });
+    }
+
+    const decoded = jwt.verify(token, config.secretKey);
+    const user = await User.findById(decoded.userId);
+    if (!user) {
+      return res.status(401).json({ message: 'User not found' });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = authMiddleware;
